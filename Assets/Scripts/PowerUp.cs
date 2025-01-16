7c5c4716bfdee2c792d2061b57db63fb3457c075
Javiermd <javiermardom@gmail.com>
2025-01-16 09:05:33 +0100

    nada
    DESACTIVAR Y ACTIVAR PANELES

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PowerUp : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody2D powerRb;
+
+    [SerializeField] private SpriteRenderer doubleShotNo;
+    [SerializeField] private SpriteRenderer doubleShot;
+
+    private void Start()
+    {
+        doubleShotNo = this.gameObject.GetComponent<SpriteRenderer>();
+        doubleShot= this.gameObject.GetComponent<SpriteRenderer>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -14,6 +25,10 @@ public class PowerUp : MonoBehaviour
 
         if (other.CompareTag("Player"))
         {
+            doubleShotNo.enabled = false;
+            doubleShot.enabled = true;
+
+
             Destroy(gameObject);
         }
 
